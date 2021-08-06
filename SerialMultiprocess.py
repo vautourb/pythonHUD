683498fb35ed72cebcc44f895e4e73bdd3da64c6
vautourb <vautourb@live.com>
2021-08-06 17:53:17 -0400

    initial

--- SerialMultiprocess.py
+++ SerialMultiprocess.py
@@ -5,20 +5,20 @@ import time
 
 
 class MySerialManager(Process):
-    def __init__(self, serial_port, baudrate=9600, timeout=1):
+    def __init__(self, serial_port, baudrate=9600, timeout=None):
         super(MySerialManager, self).__init__(target=self.loop_iterator,args=(serial_port, baudrate, timeout))
 
 
     def loop_iterator(self,serial_port, baudrate,timeout):
-        ser = serial.Serial(serial_port, baudrate=baudrate, timeout=timeout)
+        ser = serial.Serial(serial_port, baudrate=baudrate, timeout=None)
         self.loop(ser)
 
     def loop(self,ser):
 
-        for i in range(3):
+        while True:
             data_raw = ser.readline()
             print(data_raw)
-            time.sleep(1)
+
 
 
 if __name__ == "__main__":
