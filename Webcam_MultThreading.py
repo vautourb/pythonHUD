15757bc62f1726c86ed3f3c05315808b04813530
vautourb <vautourb@live.com>
2021-08-07 16:13:43 -0400

    initial

--- Webcam_MultThreading.py
+++ Webcam_MultThreading.py
@@ -8,6 +8,7 @@ cv2.namedWindow('frame', cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty('frame', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 font = cv2.FONT_HERSHEY_COMPLEX_SMALL
 
+
 class VideoStreamWidget(object):
     def __init__(self, src=0):
         self.capture = cv2.VideoCapture(src)
@@ -22,14 +23,13 @@ class VideoStreamWidget(object):
             if self.capture.isOpened():
                 (self.status, self.frame) = self.capture.read()
 
-
-
             # time.sleep(.01)
 
     def show_frame(self):
 
         # Display text on frames in main program
-        cv2.putText(self.frame, strftime("%Y-%m-%d" + "  " + "%H:%M:%S"), (175, 475), font, 1, (255, 255, 0), 1, cv2.LINE_8)
+        cv2.putText(self.frame, strftime("%Y-%m-%d" + "  " + "%H:%M:%S"), (175, 475), font, 1, (255, 255, 0), 1,
+                    cv2.LINE_8)
         cv2.putText(self.frame, 'LON : ', (5, 15), font, 1, (255, 255, 0), 1, cv2.LINE_8)
         cv2.putText(self.frame, 'LAT : ', (5, 35), font, 1, (255, 255, 0), 1, cv2.LINE_8)
         cv2.putText(self.frame, 'SPD : ', (5, 55), font, 1, (255, 255, 0), 1, cv2.LINE_8)
@@ -42,6 +42,7 @@ class VideoStreamWidget(object):
             self.capture.release()
             cv2.destroyAllWindows()
             exit(1)
+
 
 if __name__ == '__main__':
     video_stream_widget = VideoStreamWidget()
